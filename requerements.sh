53a0aaf6cfd1a2e6ec3dc0534a865cd7a8b7fb75
Andreas Lorensen <andlo@outlook.dk>
2018-10-27 08:22:59 +0000

    test

---
+++ requerements.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# The requirements.sh is an advanced mechanism an should rarely be needed.
+# Be aware that it won't run with root permissions and 'sudo' won't work
+# in most cases.
+
+#detect distribution using lsb_release (may be replaced parsing /etc/*release)
+dist=$(lsb_release -ds)
+
+#setting dependencies and package manager in relation to the distribution
+
+if $(hash pkcon 2>/dev/null); then
+    pm="pkcon"
+else
+    priv="sudo"
+    if [ "$dist"  == "\"Arch Linux\""  ]; then
+        pm="pacman -S"
+        dependencies=( aaa )
+    elif [[ "$dist" =~  "Ubuntu" ]] || [[ "$dist" =~ "Debian" ]] ||[[ "$dist" =~ "Raspbian" ]]; then
+        pm="apt install"
+        dependencies=( "nodejs", "tmux", "mc")
+    elif [[ "$dist" =~ "SUSE" ]]; then 
+        pm="zypper install"
+        dependencies=( ccc )
+    fi
+fi
+
+
+# installing dependencies
+if [ ! -z "$pm" ]; then
+    for dep in "${dependencies[@]}"
+    do
+        $priv $pm $dep
+    done
+fi
