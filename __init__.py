2e627c9aba0a9e1125fe7c1a34c717fd78c52983
Andreas Lorensen <andlo@outlook.dk>
2018-10-27 12:22:27 +0000

    test

--- __init__.py
+++ __init__.py
@@ -8,8 +8,9 @@ class CloudNine(MycroftSkill):
         MycroftSkill.__init__(self)
         
     def initialize(self):
-        os.system("/opt/mycroft/skills/code-nine-skill/c9/scripts/install-sdk.sh")
-        run("/opt/mycroft/skills/code-nine-skill/c9/server.js -p 8181 -w /home/pi/workspace -l 0.0.0.0 -a :")
+        os.system("/opt/mycroft/skills/cloud-nine-skill.andlo/c9/scripts/install-sdk.sh")
+        command = "/opt/mycroft/skills/cloud-nine-skill.andlo/c9/server.js -p 8080 -w /home/pi/workspace -l 0.0.0.0 -a :"
+        run(command.split(command)
 
     @intent_file_handler('nine.cloud.intent')
     def handle_nine_cloud(self, message):
